f527319e053e198f61c96f6d1ce81d49f20c4d78
Simon S <ssuvemaa@tktk.ee>
2022-03-09 11:36:40 +0200

    add new projects

---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
